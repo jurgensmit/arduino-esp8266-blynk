b85d5a3382a2a0c2c2287a894f432117863d9dcb
Jurgen Smit <jurgen.smit@gmail.com>
2015-07-20 22:14:19 +0200

    added comment header to the authorization include file

--- authorization.h
+++ authorization.h
@@ -1,5 +1,12 @@
+/*
+ * Arduino ESP8266 Blynk - Use the Blynk IoT platform to create an Internet connected temperature sensor
+ *
+ * This file contains the authorization information for both your local WiFi network as well
+ * as the authorization token of the Blynk project. Replace them with your own values.
+ * 
+ * (c) 2015 Jurgen Smit. All rights reserved.
+ */
+ 
 #define BLYNK_TOKEN         "<Blync Token>"
 #define WIFI_SSID           "<WiFi SSID>"
 #define WIFI_PASSWORD       "<WiFi Password>"
-
-
