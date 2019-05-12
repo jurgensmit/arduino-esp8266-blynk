761a15b5d07ebf1d3244b793acb4b60f0eec563c
Jurgen Smit <jursmit_admin@deloitte.nl>
2019-05-12 08:33:21 +0200

    Updated for new external libraries and NodeMCU

--- arduino-esp8266-blynk.ino
+++ arduino-esp8266-blynk.ino
@@ -1,98 +1,110 @@
 /*
  * Arduino ESP8266 Blynk - Use the Blynk IoT platform to create an Internet connected temperature sensor
  *
- * This sketch will regularly measure the temperature and the humidity of the DHT22 sensor and push it
- * to the Blynk IoT platform (http://www.blynk.cc/). We can use the Blynk phone app to monitor the temperature
- * and humidity wherever we are.
- * The sketch will measure the temperature and humidity every minute and send a running average of the last 15
- * samples to the blync server.
- * A blynk project was created on an iPhone which displays the temperature and humidity in different forms,
- * as gauge, as value and as graph. Next to that a watchdog led is shown which blinks every second. A real
- * led is also present connected to the arduino which should blink with the same frequency. This provides
- * an easy way to see if the project is alive. Another virtual led in the blynk project will light up when
- * there was an error reading the temperature or humidity from the DHT22. Before I was using the Adafruit DHT
- * library which was given a lot of (crc) errors in combination with the DHT22. It looks like the timing is
- * slighty off. I did not have the same issues with the DHT sensor library (see external references). 
- * Furthermore the number of samples the mean was based is displayed. This was also used to investigate the
+ * This sketch will regularly measure the temperature and the humidity of the DHT22 sensor and the light 
+ * intensity as measured by a photoresistor and push it to the Blynk IoT platform (http://www.blynk.cc/). 
+ * We can use the Blynk phone app to monitor the temperature, humidity and light intensity wherever we are.
+ * The sketch will measure the temperature, humidity and light intensity every minute and send it to the 
+ * blync server as well as a running average of the last 15 samples.
+ * A blynk project was created on an iPhone which displays the temperature, humidity and light intensity in 
+ * different forms, as gauge, as value and as graph. Next to that a watchdog led is shown which blinks every 
+ * second, a real led is also present connected to the arduino which should blink with the same frequency. 
+ * This provides an easy way to see if the project is alive. Another virtual led in the blynk project will 
+ * light up when there was an error reading the temperature or humidity from the DHT22. 
+ * The number of samples the mean is based on is displayed. This is used to investigate the
  * stability of the sensor library. Normally this value is 15, the size of the running average buffer, but 
  * can become less when samples are missing when the temperature or humidity cannot be read from the DHT
- * sensor. This last value is pulled from the Blynk project to wake it up after it got disconnected when
- * the iPhone goes to sleep.
- * 
+ * sensor. 
+ *  
  * Please note that you have to replace the values in the file authorization.h with your own values!
  * 
- * (c) 2015 Jurgen Smit. All rights reserved.
+ * (c) 2015-2019 Jurgen Smit. All rights reserved.
  *
  * Circuit: 
  *
- * - ESP8266 Wifi Module connected to the second serial port of the Arduino.
- * - DHT22 Temperature sensor connected to pin 4
- * - An optional LED connected to digital pin 9 that can be controlled from blynk
- * - An optional LED connected to digital pin 8 that will blink every second
+ * - NodeMCU 0.9 module
+ * - DHT22 Temperature sensor connected to digital pin D1 (GPIO5)
+ * - Photoresistor connected to analog pin A0 (ADC0)
+ * - An optional LED connected to digital pin D4 (GPIO2) that will blink every second
  * 
  * External References:
  * 
  * - WeeESP8266 - An ESP8266 library for Arduino: https://github.com/itead/ITEADLIB_Arduino_WeeESP8266
  * - Blynk library: https://github.com/blynkkk/blynk-library 
- * - DHT22 Sensor Library: https://github.com/nethoncho/Arduino-DHT22
+ * - Adafruit DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+ * - Adafruit Unified Sensor Driver: https://github.com/adafruit/Adafruit_Sensor
  * - SimpleTimer Library for Arduino: http://playground.arduino.cc/Code/SimpleTimer
  * 
  * Blynk Project:
  * 
- * - Gauge: Temperature - V0 - Push
- * - Gauge: Humidity - V1 - Push
- * - Display: Temperature - V4 - Push
+ * - Gauge: Average Temperature - V0 - Push
+ * - Gauge: Average Humidity - V1 - Push
+ * - Gauge: Average Light Intensity - V2 - Push
+ * - Display: Current Temperature - V3 - Push
+ * - Display: Current Humidity - V4 - Push
+ * - Display: Current Light Intensity - V5 - Push
  * - Led: Watchdog - V30 - Push
  * - Led: Error reading DTH22 - V31 - Push
- * - Display: Humidity - V5 - Push
- * - Display: Number of valid samples in mean temperature and humidity - V29 - Refresh every 30 seconds
- * - Graph: Temperature - V2 - Push
- * - Graph: Humidity - V3 - Push
- * - Slider: Led - D9
+ * - Display: Number of valid samples in mean values - V29 - Push
  */
 
+// Uncomment the following lines for debugging purposes when needed
 //#define BLYNK_DEBUG
-//#define BLYNK_PRINT Serial   
+//#define BLYNK_PRINT Serial  
 
-#include <ESP8266.h>
-#include <BlynkSimpleShieldEsp8266.h>
-#include <DHT22.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <DHT.h>
 #include <SimpleTimer.h>
 
 #include "authorization.h"
 
-#define EspSerial           Serial1   // Use Serial1 to communicate to the ESP8266
-#define DHT_PIN             4         // Arduino pin connected to the DHT temperature and humidity sensor
-#define WATCHDOG_PIN        8         // Watchdog led which should blink every second
+#define DHT_PIN               5         // Arduino pin connected to the temperature and humidity sensor
+#define DHT_TYPE              DHT22     // The type of DHT sensor, supported are DHT11, DHT22 and AM2302
+#define WATCHDOG_PIN          4         // Watchdog led which should blink every second
+#define PHOTORESISTOR_PIN     0         // Arduino pin connected to the photoresistor
 
-#define SAMPLE_INTERVAL     15        // Send the running average of 15 minutes to the cloud
+#define VLED_WATCHDOG         30        // Blynk LED to indicate heart beat 
+#define VLED_ERROR            31        // Blynk LED to indicate an error 
 
-ESP8266 wifi(EspSerial);
-DHT22 dht(DHT_PIN);
+#define VPORT_AVGTEMPERATURE  0         // Blynk value for the average temperature
+#define VPORT_AVGHUMIDITY     1         // Blynk value for the average humidity
+#define VPORT_AVGLIGHT        2         // Blynk value for the average light intensity
+#define VPORT_TEMPERATURE     3         // Blynk value for the current temperature
+#define VPORT_HUMIDITY        4         // Blynk value for the current humidity
+#define VPORT_LIGHT           5         // Blynk value for the current light intensity
+#define VPORT_SAMPLECOUNT     29        // Blynk value that represents the number of values that
+                                        // was used to calculate the average values
+
+#define LED_ON                255       // Value to turn LED on
+#define LED_OFF               0         // Value to turn LED off
+
+#define SAMPLE_INTERVAL       15        // Send the running average of 15 minutes to the cloud
+
+DHT dht(DHT_PIN, DHT_TYPE);
 SimpleTimer timer;
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-const char auth[] = BLYNK_TOKEN;
-
+// Buffers to calculate the average values
 float temperatureBuffer[SAMPLE_INTERVAL];
 float humidityBuffer[SAMPLE_INTERVAL];
+float lightBuffer[SAMPLE_INTERVAL];
 
 void setup() {
+  // Watchdog LED
   pinMode(WATCHDOG_PIN, OUTPUT);
+  
+  // Temperature and humidity sensor
+  dht.begin();
+  
+  // Connection to the Blynk server
+  Blynk.begin(BLYNK_TOKEN, WIFI_SSID, WIFI_PASSWORD);
+  while (!Blynk.connect()) {
+    // Wait until connected
+  }
 
-  // Initialize the serial ports
-  EspSerial.begin(115200);  // Set ESP8266 baud rate
-  delay(10);
-  Serial.begin(115200);
-  delay(10);
-
-  // Initialize the Blynk libraries
-  Blynk.begin(auth, wifi, WIFI_SSID, WIFI_PASSWORD);
-
-  // Initialize the running buffer
+  // Running buffer to calculate the aveage values
   for(int i = 0; i <= SAMPLE_INTERVAL; i ++) {
-    temperatureBuffer[i] = humidityBuffer[i] = NAN;
+    temperatureBuffer[i] = humidityBuffer[i] = lightBuffer[i] = NAN;
   }
 
   // Initial sending of the sensor values
@@ -101,7 +113,7 @@ void setup() {
   // Sample the sensors every minute
   timer.setInterval(60L * 1000L, sampleSensorValues);
   
-  // Toggle a led every second
+  // Toggle the watchdog LED every second
   timer.setInterval(1000L, watchDog);
 }
 
@@ -110,8 +122,15 @@ void setup() {
  * on the iPhone dashboard
  */
 void watchDog() {
-  digitalWrite(WATCHDOG_PIN, !digitalRead(WATCHDOG_PIN));
-  Blynk.virtualWrite(30, digitalRead(WATCHDOG_PIN));
+  BLYNK_LOG("Watch Dog");
+  if(digitalRead(WATCHDOG_PIN)) {
+    digitalWrite(WATCHDOG_PIN, 0);
+    Blynk.virtualWrite(VLED_WATCHDOG, LED_ON);
+  }
+  else {
+    digitalWrite(WATCHDOG_PIN, 1);
+    Blynk.virtualWrite(VLED_WATCHDOG, LED_OFF);
+  }
 }
 
 /*
@@ -139,121 +158,143 @@ float calculateMean(float values[], int n) {
 
 int sampleIndex = 0;
 long totalSamples = 0;
+float temperature = NAN;
+float humidity = NAN;
+float light = NAN;
 
 /*
  * Sample the sensor values and send them to the Blynk server
  */
 void sampleSensorValues() {
 
-  float temperature = NAN;
-  float humidity = NAN;
+  BLYNK_LOG("Sample Sensor Values...");
 
-  DHT22_ERROR_t errorCode = dht.readData();
-  if(errorCode == DHT_ERROR_NONE) {
-    temperature = dht.getTemperatureC();
-    humidity = dht.getHumidity();
+  if(dht.read()) {
+    temperature = dht.readTemperature();
+    humidity = dht.readHumidity();
+    light = analogRead(PHOTORESISTOR_PIN);
+
+    Blynk.virtualWrite(VLED_ERROR, LED_OFF);
+
+    BLYNK_LOG("    Temperature: " + String(temperature));
+    BLYNK_LOG("    Humidity: " + String(humidity));
+    BLYNK_LOG("    Light: " + String(light));
+  }
+  else {
+    BLYNK_LOG("Error reading sensor values!");
+    Blynk.virtualWrite(VLED_ERROR, LED_ON);
   }
 
   temperatureBuffer[sampleIndex] = temperature;
   humidityBuffer[sampleIndex] = humidity;
+  lightBuffer[sampleIndex] = light;
 
   sampleIndex ++;
   if(sampleIndex >= SAMPLE_INTERVAL) {
     sampleIndex = 0;
   }
 
-  sendTemperature();
   sendHumidity();
+  sendTemperature();
+  sendLight();
   sendSampleCount();
-
-  if(isnan(temperature) || isnan(humidity)) {
-      Blynk.virtualWrite(31, HIGH);
-  }
-  else {
-      Blynk.virtualWrite(31, LOW);
-  }
 }
 
 /*
- * Send the temperature to the blynk server. We have to send it several times 
- * as we can only bind a (virtual) port to a single widget in the Blynk project.
+ * Send the temperature (current and average) to the blynk server. 
  */
 void sendTemperature() {
+  BLYNK_LOG("Send Temperature");
   float meanTemperature = calculateMean(temperatureBuffer, SAMPLE_INTERVAL);
   if(!isnan(meanTemperature)) {
-    Blynk.virtualWrite(0, meanTemperature);
-    Blynk.virtualWrite(2, meanTemperature);
-    Blynk.virtualWrite(4, meanTemperature);
+    Blynk.virtualWrite(VPORT_AVGTEMPERATURE, meanTemperature);
+  }
+  if(!isnan(temperature)) {
+    Blynk.virtualWrite(VPORT_TEMPERATURE, temperature);
   }
 }
 
 /*
- * Send the humidity to the blynk server. We have to send it several times 
- * as we can only bind a (virtual) port to a single widget in the Blynk project.
+ * Send the humidity (current and average) to the blynk server. 
  */
 void sendHumidity() {
+  BLYNK_LOG("Send Humidity");
   float meanHumidity = calculateMean(humidityBuffer, SAMPLE_INTERVAL);
   if(!isnan(meanHumidity)) {
-    Blynk.virtualWrite(1, meanHumidity);
-    Blynk.virtualWrite(3, meanHumidity);
-    Blynk.virtualWrite(5, meanHumidity);
+    Blynk.virtualWrite(VPORT_AVGHUMIDITY, meanHumidity);
+  }
+  if(!isnan(humidity)) {
+    Blynk.virtualWrite(VPORT_HUMIDITY, humidity);
+  }
+}
+
+/*
+ * Send the light intensity (current and average) to the blynk server. 
+ */
+void sendLight() {
+  BLYNK_LOG("Send Light");
+  float meanLight = calculateMean(lightBuffer, SAMPLE_INTERVAL);
+  if(!isnan(meanLight)) {
+    Blynk.virtualWrite(VPORT_AVGLIGHT, meanLight);
+  }
+  if(!isnan(light)) {
+    Blynk.virtualWrite(VPORT_LIGHT, light);
   }
 }
 
 /*
  * This function is invoked when the Blynk server request the value for
- * the virtual port V0. It returns the running average of the temperature.
+ * the running average of the temperature.
  */
-BLYNK_READ(0) {
+BLYNK_READ(VPORT_AVGTEMPERATURE) {
   sendTemperature();
 }
 
 /*
  * This function is invoked when the Blynk server request the value for
- * the virtual port V2. It returns the running average of the temperature.
+ * the running average of the humidity.
  */
-BLYNK_READ(2) {
+BLYNK_READ(VPORT_AVGHUMIDITY) {
+  sendHumidity();
+}
+
+/*
+ * This function is invoked when the Blynk server request the value for
+ * the running average of the light intensity.
+ */
+BLYNK_READ(VPORT_AVGLIGHT) {
+  sendLight();
+}
+
+/*
+ * This function is invoked when the Blynk server request the value for
+ * the current temperature.
+ */
+BLYNK_READ(VPORT_TEMPERATURE) {
   sendTemperature();
 }
 
 /*
  * This function is invoked when the Blynk server request the value for
- * the virtual port V4. It returns the running average of the temperature.
+ * the current humidity.
  */
-BLYNK_READ(4) {
-  sendTemperature();
-}
-
-/*
- * This function is invoked when the Blynk server request the value for
- * the virtual port V1. It returns the running average of the humidity.
- */
-BLYNK_READ(1) {
+BLYNK_READ(VPORT_HUMIDITY) {
   sendHumidity();
 }
 
 /*
  * This function is invoked when the Blynk server request the value for
- * the virtual port V3. It returns the running average of the humidity.
+ * the current light intensity.
  */
-BLYNK_READ(3) {
-  sendHumidity();
+BLYNK_READ(VPORT_LIGHT) {
+  sendLight();
 }
 
 /*
- * This function is invoked when the Blynk server request the value for
- * the virtual port V5. It returns the running average of the humidity.
+ * This function is invoked when the Blynk server request the value the number of valid 
+ * samples in the buffers to calculate the average values.
  */
-BLYNK_READ(5) {
-  sendHumidity();
-}
-
-/*
- * This function is invoked when the Blynk server request the value for
- * the virtual port V29. It returns the number of valid samples in the temperature
- * and humidity buffers.
- */
-BLYNK_READ(29) {
+BLYNK_READ(VPORT_SAMPLECOUNT) {
   sendSampleCount();
 }
 
@@ -261,17 +302,17 @@ BLYNK_READ(29) {
  * Send the number of valid values in the running average buffers.
  */
 void sendSampleCount() {
+  BLYNK_LOG("Send Sample Count");
   int sampleCount = 0;
   for(int i = 0; i < SAMPLE_INTERVAL; i ++) {
     if(!isnan(temperatureBuffer[i]) && !isnan(humidityBuffer[i])) {
       sampleCount++;
     }
   }
-  Blynk.virtualWrite(29, sampleCount);
+  Blynk.virtualWrite(VPORT_SAMPLECOUNT, sampleCount);
 }
 
 void loop() {
   Blynk.run();
   timer.run();
 }
-
